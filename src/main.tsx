2aecce83372d8b672b456eb70d4a7a9702358873
garvsl <garvsehgal7@gmail.com>
2024-01-12 18:26:47 -0500

    sample design

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Theme>
+    <Theme  accentColor="grass" grayColor="sand" radius="large" scaling="95%">
       <App />
     </Theme>
   </React.StrictMode>,
